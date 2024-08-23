4f4ed34ee0dcb0a0e4d6a4f7bf97f0d6efdb1d7b
MinuteMotionStudios <MinuteMotionStudios@proton.me>
2024-08-23 15:34:26 +0000

    Add files via upload

---
+++ JS/idex.js
@@ -0,0 +1,19 @@
+ document.getElementById("travel_article_button").addEventListener('click', function() {
+    window.location.href = 'travel_artical.html'; 
+  });
+
+ document.getElementById("home_button").addEventListener('click', function() {
+    window.location.href = 'index.html';
+  });
+
+ document.getElementById("reviews_button").addEventListener('click', function() {
+    window.location.href = 'https://maps.app.goo.gl/jEujX5Jh31XjiNMs5';
+  });
+
+ document.getElementById("blog_button").addEventListener("click", function() {
+    window.location.href = "blog.html";
+})
+
+document.getElementById("book_a_tour_button").addEventListener("click", function() {
+   window.location.href = "book_a_tour.html";
+})
