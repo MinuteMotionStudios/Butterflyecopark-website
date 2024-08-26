99ff998cc965c5a18da3900fc0e1ce91a971bcbe
MinuteMotionStudios <MinuteMotionStudios@proton.me>
2024-08-26 20:31:19 +0000

    Add files via upload

---
+++ JS/idex.js
@@ -0,0 +1,15 @@
+ document.getElementById("travel_article_button").addEventListener('click', function() {
+    window.location.href = 'travel_artical.html'; 
+  });
+
+ document.getElementById("home_button").addEventListener('click', function() {
+    window.location.href = 'index.html';
+  });
+
+ document.getElementById("reviews_button").addEventListener('click', function() {
+    window.location.href = 'https://maps.app.goo.gl/jEujX5Jh31XjiNMs5';
+  });
+
+ document.getElementById("blog_button").addEventListener("click", function() {
+    window.location.href = "https://lisakile.blogspot.com/";
+})
